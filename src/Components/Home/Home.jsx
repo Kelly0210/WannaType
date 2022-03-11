45035c07efa0c905ae40b334a3892be494ac11ca
Alan Keller <52039446+Kelly0210@users.noreply.github.com>
2022-03-11 18:57:58 +0600

    a little bit CSS for home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './Home.module.css';
 
 const Home = () => {
     return (
-        <div>Hello this is Home page!</div>
+        <div className={style.homeContainer}>
+            <p>Hello this is Home page!</p>
+            <p>Start <Link to='/training' className={style.linkHome}>typing</Link></p>
+            </div>
     )
 }
 
