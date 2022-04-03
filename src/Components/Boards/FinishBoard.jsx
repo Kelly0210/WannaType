98a02b9d4ef1eff698e43ede98bf16c3573a3d91
Alan Keller <52039446+Kelly0210@users.noreply.github.com>
2022-04-03 23:38:59 +0600

    heart indicator, links, images

--- src/Components/Boards/FinishBoard.jsx
+++ src/Components/Boards/FinishBoard.jsx
@@ -3,19 +3,24 @@ import style from './FinishBoard.module.css';
 import reload from '../../assets/img/reload.png';
 import arrowRight from '../../assets/img/arrowRight.png';
 import arrowLeft from '../../assets/img/arrowLeft.png';
+import { Link } from 'react-router-dom';
 
 
 const FinishBoard = (props) => {
+
     const reloadLesson = () => {
         props.reloadLesson();
     }
 
     return (
         <div className={style.boardBox}>
+
         {props.result ? <h2 className={style.result}>Congratulation!</h2> : <h2 className={style.result}>Try again?</h2>}
         <img src={reload} onClick={reloadLesson} className={style.reloadIcon}/>
-        <img className={style.arrowRight} src={arrowRight}/>
-        <img className={style.arrowLeft} src={arrowLeft}/>
+        
+        <Link to='training/lesson-1/chapter-1' className={style.arrowLeftBox}><img src={arrowLeft}/></Link>
+        <Link to={props.result ? 'training/lesson-1/chapter-1' : '#'} className={style.arrowRightBox}><img src={arrowRight}/></Link>
+
         </div>
         
     )
--- src/Components/StatusBar/StatusBar.jsx
+++ src/Components/StatusBar/StatusBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './StatusBar.module.css';
+import heart from '../../assets/img/heart.png'
 
 class StatusBar extends React.Component {
 
@@ -7,12 +8,26 @@ class StatusBar extends React.Component {
         super(props)
     }
 
+    componentDidUpdate() {
+        switch (this.props.numberOfMistake) {
+            case 1: document.getElementById('heart-1')?.remove(); break;
+            case 2: document.getElementById('heart-2')?.remove(); break;
+            case 3: document.getElementById('heart-3')?.remove(); break;
+        }
+    }
+
     render() {
+
         return (
             <div className={style.statusBox}>
-                <span>Number of mistakes: {this.props.numberOfMistake}</span>
-                <span> Process: {this.props.percentage}%</span>
-                <span> CPM: {this.props.cpm}</span>
+                {/* <span>Number of mistakes: {this.props.numberOfMistake} </span> */}
+                <img id='heart-1' src={heart} className={style.heart} />
+                <img id='heart-2' src={heart} className={style.heart} />
+                <img id='heart-3' src={heart} className={style.heart} />
+
+                <span className={style.percentageBox}> Process: {this.props.percentage}%</span>
+                <span className={style.cpmBox}> CPM: {this.props.cpm}</span>
+                <div className={style.heart}></div>
             </div>
         )
     }
