2275dd76fe2d2d48d476a0ac538361b52d5d3697
Alan Keller <52039446+Kelly0210@users.noreply.github.com>
2022-04-06 01:36:37 +0600

    almost all lesson

--- src/Components/common/Lessons.js
+++ src/Components/common/Lessons.js
@@ -11,6 +11,7 @@ export const Lesson1 = {
     chapter7: ['d', 'f', 'j', 'k'],
     chapter8: ['d', 'f', 'j', 'k'],
     chapter9: ['d', 'f', 'j', 'k'],
+
 };
 
 export const Lesson2 = {
@@ -26,250 +27,39 @@ export const Lesson2 = {
     chapter8: ['a', ';'],
     chapter9: ['a', ';'],
 
-    chapter10: ['a', 's', 'd', 'f', 'j', 'k', 'l', ';'],
-    chapter11: ['a', 's', 'd', 'f', 'j', 'k', 'l', ';'],
-    chapter12: ['a', 's', 'd', 'f', 'j', 'k', 'l', ';'],
+    chapter10: [],
+    chapter11: [],
+    chapter12: [],
+
+    chapter13: [],
+    chapter14: [],
 };
 
 export const Lesson3 = {
-    chapter1: ['g', 'h'],
-    chapter2: ['g', 'h'],
-    chapter3: ['g', 'h'],
+    chapter1: [],
+    chapter2: [],
+    chapter3: [],
+    chapter4: [],
+    chapter5: [],
+    chapter6: [],
+    chapter7: [],
 
-    chapter4: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter5: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter6: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-
-    chapter7: ['gas', 'lags', 'has',],
     chapter8: [],
     chapter9: [],
+    chapter10: [],
+    chapter11: [],
+    chapter12: [],
+    chapter13: [],
+    chapter14: [],
 };
-
 // Second row
 
-export const Lesson4 = {
-    chapter1: ['r', 'u'],
-    chapter2: ['r', 'u'],
-    chapter3: ['r', 'u'],
 
-    chapter4: ['r', 'f', 'j', 'u'],
-    chapter5: ['r', 'f', 'j', 'u'],
-    chapter6: ['r', 'f', 'j', 'u'],
-
-    chapter7: ['r', 'u',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter8: ['r', 'u',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter9: ['r', 'u',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-
-    chapter10: []
-};
-
-export const Lesson5 = {
-    chapter1: ['t', 'y'],
-    chapter2: ['t', 'y'],
-    chapter3: ['t', 'y'],
-
-    chapter4: ['r', 't', 'f', 'j', 'y', 'u'],
-    chapter5: ['r', 't', 'f', 'j', 'y', 'u'],
-    chapter6: ['r', 't', 'f', 'j', 'y', 'u'],
-
-    chapter7: ['r', 't', 'y', 'u',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter8: ['r', 't', 'y', 'u',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter9: ['r', 't', 'y', 'u',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-
-    chapter10: []
-};
-
-export const Lesson6 = {
-    chapter1: ['e', 'i'],
-    chapter2: ['e', 'i'],
-    chapter3: ['e', 'i'],
-
-    chapter4: ['e', 'd', 'k', 'i'],
-    chapter5: ['e', 'd', 'k', 'i'],
-    chapter6: ['e', 'd', 'k', 'i'],
-
-    chapter7: ['e', 'r', 't', 'y', 'u', 'i',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter8: ['e', 'r', 't', 'y', 'u', 'i',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter9: ['e', 'r', 't', 'y', 'u', 'i',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-
-    chapter10: []
-};
-
-export const Lesson7 = {
-    chapter1: ['w', 'o'],
-    chapter2: ['w', 'o'],
-    chapter3: ['w', 'o'],
-
-    chapter4: ['w', 's', 'o', 'l'],
-    chapter5: ['w', 's', 'o', 'l'],
-    chapter6: ['w', 's', 'o', 'l'],
-
-    chapter7: ['w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter8: ['w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter9: ['w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-
-    chapter10: []
-};
-
-export const Lesson8 = {
-    chapter1: ['q', 'p'],
-    chapter2: ['q', 'p'],
-    chapter3: ['q', 'p'],
-
-    chapter4: ['q', 'a', ';', 'p'],
-    chapter5: ['q', 'a', ';', 'p'],
-    chapter6: ['q', 'a', ';', 'p'],
-
-    chapter7: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter8: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-    chapter9: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
-
-    chapter10: []
-};
-
-//Third row
-
-export const Lesson9 = {
-    chapter1: ['v', 'm'],
-    chapter2: ['v', 'm'],
-    chapter3: ['v', 'm'],
-
-    chapter4: ['v', 'f', 'j', 'm'],
-    chapter5: ['v', 'f', 'j', 'm'],
-    chapter6: ['v', 'f', 'j', 'm'],
-
-    chapter7: ['r', 'v', 'f', 'j', 'm', 'u'],
-    chapter8: ['r', 'v', 'f', 'j', 'm', 'u'],
-    chapter9: ['r', 'v', 'f', 'j', 'm', 'u'],
-
-    chapter10: ['r', 't', 'v', 'f', 'j', 'm', 'u', 'y'],
-    chapter11: ['r', 't', 'v', 'f', 'j', 'm', 'u', 'y'],
-    chapter12: ['r', 't', 'v', 'f', 'j', 'm', 'u', 'y'],
-
-    chapter13: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-        'v', 'm'],
-    chapter14: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-        'v', 'm'],
-    chapter15: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-        'v', 'm'],
-
-    chapter16: []
-
-};
-
-export const Lesson10 = {
-    chapter1: ['c', ','],
-    chapter2: ['c', ','],
-    chapter3: ['c', ','],
-
-    chapter4: ['d', 'c', ',', 'k'],
-    chapter5: ['d', 'c', ',', 'k'],
-    chapter6: ['d', 'c', ',', 'k'],
-
-    chapter7: ['e', 'd', 'c', ',', 'k', 'i'],
-    chapter8: ['e', 'd', 'c', ',', 'k', 'i'],
-    chapter9: ['e', 'd', 'c', ',', 'k', 'i'],
-
-    chapter10: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-        'c', 'v', 'm', ','],
-    chapter11: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-        'c', 'v', 'm', ','],
-    chapter12: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-        'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-        'c', 'v', 'm', ','],
-
-    chapter13: []
-};
-
-export const Lesson11 = {
-    chapter1: ['x', '.'],
-    chapter2: ['x', '.'],
-    chapter3: ['x', '.'],
-
-    chapter4: ['s', 'x', '.', 'l'],
-    chapter5: ['s', 'x', '.', 'l'],
-    chapter6: ['s', 'x', '.', 'l'],
-
-    chapter7: ['w', 's', 'x', '.', 'l', 'o'],
-    chapter8: ['w', 's', 'x', '.', 'l', 'o'],
-    chapter9: ['w', 's', 'x', '.', 'l', 'o'],
-
-    chapter10: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-    'x', 'c', 'v', 'm', ',', '.'],
-    chapter11: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-    'x', 'c', 'v', 'm', ',', '.'],
-    chapter12: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-    'x', 'c', 'v', 'm', ',', '.'],
-
-    chapter13: []
-};
-
-export const Lesson12 = {
-    chapter1: ['z', '/'],
-    chapter2: ['z', '/'],
-    chapter3: ['z', '/'],
-
-    chapter4: ['a', 'z', '/', ';'],
-    chapter5: ['a', 'z', '/', ';'],
-    chapter6: ['a', 'z', '/', ';'],
-
-    chapter7: ['q', 'a', 'z', '/', ';', 'p'],
-    chapter8: ['q', 'a', 'z', '/', ';', 'p'],
-    chapter9: ['q', 'a', 'z', '/', ';', 'p'],
-
-    chapter10: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-    'z', 'x', 'c', 'v', 'm', ',', '.', '/'],
-    chapter11: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-    'z', 'x', 'c', 'v', 'm', ',', '.', '/'],
-    chapter12: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
-    'z', 'x', 'c', 'v', 'm', ',', '.', '/'],
-
-    chapter13: []
-};
 // export const Lessonn2 = ['d', 'j', 'k', 'l'];
 // export const Lesson3 = ['s', 'd', 'j', 'k', 'l', ';'];
 // export const Lesson4 = ['a','s', 'd', 'j', 'k', 'l', ';',"'"];
 
-export const LessonExmp = {
-    chapter1: [],
-    chapter2: [],
-    chapter3: [],
-
-    chapter4: [],
-    chapter5: [],
-    chapter6: [],
-
-    chapter7: [],
-    chapter8: [],
-    chapter9: [],
-
-    chapter10: [],
-    chapter11: [],
-    chapter12: []
-};
+// export const Lesson5 = ['q', 'i'];
+// export const Lesson6 = ['w', 'q', 'i', 'o'];
+// export const Lesson7 = ['s', 'd', 'j', 'k', 'l', ';'];
+// export const Lesson8 = ['a','s', 'd', 'j', 'k', 'l', ';',"'"];
