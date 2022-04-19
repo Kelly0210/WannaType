024398c268881fc1f6f7789ce4087340c0d2c725
Alan Keller <52039446+Kelly0210@users.noreply.github.com>
2022-04-19 18:49:33 +0600

    Flashlight on keyboard

--- src/Components/Keyboard/Keyboard.jsx
+++ src/Components/Keyboard/Keyboard.jsx
@@ -5,86 +5,86 @@ import * as PropTypes from 'prop-types';
 
 class Keyboard extends React.Component {
 
-    // componentDidUpdate() {
-    //     let { lastLetter, mistakeHappend } = this.props;
-    //     let pressedButton = document.getElementById(lastLetter);
+    componentDidUpdate() {
+        let pressedButton = document.getElementById(this.props.lastLetter);
+        console.log(pressedButton)
 
-    //     if (mistakeHappend) {
-    //         pressedButton.classList.add(`${style.wrongKey}`);
-    //         setTimeout(() => { pressedButton.classList.remove(`${style.wrongKey}`) }, 1500);
-    //     }
-    //     else {
-    //         pressedButton.classList.add(`${style.correctKey}`);
-    //         setTimeout(() => { pressedButton.classList.remove(`${style.correctKey}`) }, 1500);
-    //     }
-    // }
+        pressedButton.classList.remove(`${style.wrongKey}`);
+        pressedButton.classList.remove(`${style.correctKey}`);
+        
+        if (this.props.mistakeHappened) {
+            setTimeout(() => {pressedButton.classList.add(`${style.wrongKey}`)}, 0);
+        } else {
+            setTimeout(() => {pressedButton.classList.add(`${style.correctKey}`)}, 0);
+        }
+    }
 
     render() {
         return (
             <div className={style.keyboardContainer}>
                 <div className={style.row}>
-                    <div id='`' className={`${style.key} ${style.keyWaveBlue}`}>`</div>
-                    <div id='1' className={`${style.key} ${style.keyGreen}`}>1</div>
-                    <div id='2' className={`${style.key} ${style.keyGreen}`}>2</div>
-                    <div id='3' className={`${style.key} ${style.keyBlue}`}>3</div>
-                    <div id='4' className={`${style.key} ${style.keyPink}`}>4</div>
-                    <div id='5' className={`${style.key} ${style.keyOrange}`}>5</div>
-                    <div id='6' className={`${style.key} ${style.keyOrange}`}>6</div>
-                    <div id='7' className={`${style.key} ${style.keyYellow}`}>7</div>
-                    <div id='8' className={`${style.key} ${style.keyPink}`}>8</div>
-                    <div id='9' className={`${style.key} ${style.keyBlue}`}>9</div>
-                    <div id='0' className={`${style.key} ${style.keyGreen}`}>0</div>
-                    <div id='-' className={`${style.key} ${style.keyGreen}`}>-</div>
-                    <div id='=' className={`${style.key} ${style.keyGreen}`}>=</div>
-                    <div className={`${style.key} ${style.keyBackSpace} ${style.keyWaveBlue}`}>←</div>
+                    <div id='`' className={`${style.key} ${style.FirstBlock}`}>`</div>
+                    <div id='1' className={`${style.key} ${style.secondBlock}`}>1</div>
+                    <div id='2' className={`${style.key} ${style.secondBlock}`}>2</div>
+                    <div id='3' className={`${style.key} ${style.thirdBlock}`}>3</div>
+                    <div id='4' className={`${style.key} ${style.fourthBlock}`}>4</div>
+                    <div id='5' className={`${style.key} ${style.fifthBlock}`}>5</div>
+                    <div id='6' className={`${style.key} ${style.fifthBlock}`}>6</div>
+                    <div id='7' className={`${style.key} ${style.sixthBlock}`}>7</div>
+                    <div id='8' className={`${style.key} ${style.fourthBlock}`}>8</div>
+                    <div id='9' className={`${style.key} ${style.thirdBlock}`}>9</div>
+                    <div id='0' className={`${style.key} ${style.secondBlock}`}>0</div>
+                    <div id='-' className={`${style.key} ${style.secondBlock}`}>-</div>
+                    <div id='=' className={`${style.key} ${style.secondBlock}`}>=</div>
+                    <div className={`${style.key} ${style.keyBackSpace} ${style.FirstBlock}`}>←</div>
                 </div>
                 <div className={style.row}>
-                    <div className={`${style.key} ${style.keyTab} ${style.keyWaveBlue}`}>TAB</div>
-                    <div id='q' className={`${style.key} ${style.keyGreen}`}>Q</div>
-                    <div id='w' className={`${style.key} ${style.keyBlue}`}>W</div>
-                    <div id='e' className={`${style.key} ${style.keyPink}`}>E</div>
-                    <div id='r' className={`${style.key} ${style.keyOrange}`}>R</div>
-                    <div id='t' className={`${style.key} ${style.keyOrange}`}>T</div>
-                    <div id='y' className={`${style.key} ${style.keyYellow}`}>Y</div>
-                    <div id='u' className={`${style.key} ${style.keyYellow}`}>U</div>
-                    <div id='i' className={`${style.key} ${style.keyPink}`}>I</div>
-                    <div id='o' className={`${style.key} ${style.keyBlue}`}>O</div>
-                    <div id='p' className={`${style.key} ${style.keyGreen}`}>P</div>
-                    <div id='[' className={`${style.key} ${style.keyGreen}`}>[</div>
-                    <div id=']' className={`${style.key} ${style.keyGreen}`}>]</div>
-                    <div id='\' className={`${style.key} ${style.keyWaveBlue}`}>\</div>
+                    <div className={`${style.key} ${style.keyTab} ${style.FirstBlock}`}>TAB</div>
+                    <div id='q' className={`${style.key} ${style.secondBlock}`}>Q</div>
+                    <div id='w' className={`${style.key} ${style.thirdBlock}`}>W</div>
+                    <div id='e' className={`${style.key} ${style.fourthBlock}`}>E</div>
+                    <div id='r' className={`${style.key} ${style.fifthBlock}`}>R</div>
+                    <div id='t' className={`${style.key} ${style.fifthBlock}`}>T</div>
+                    <div id='y' className={`${style.key} ${style.sixthBlock}`}>Y</div>
+                    <div id='u' className={`${style.key} ${style.sixthBlock}`}>U</div>
+                    <div id='i' className={`${style.key} ${style.fourthBlock}`}>I</div>
+                    <div id='o' className={`${style.key} ${style.thirdBlock}`}>O</div>
+                    <div id='p' className={`${style.key} ${style.secondBlock}`}>P</div>
+                    <div id='[' className={`${style.key} ${style.secondBlock}`}>[</div>
+                    <div id=']' className={`${style.key} ${style.secondBlock}`}>]</div>
+                    <div id='\' className={`${style.key} ${style.FirstBlock}`}>\</div>
                 </div>
                 <div className={style.row}>
-                    <div className={`${style.key} ${style.keyCaps} ${style.keyWaveBlue}`}>CAPS</div>
-                    <div id='a' className={`${style.key} ${style.keyGreen}`}>A</div>
-                    <div id='s' className={`${style.key} ${style.keyBlue}`}>S</div>
-                    <div id='d' className={`${style.key} ${style.keyPink}`}>D</div>
-                    <div id='f' className={`${style.key} ${style.keyOrange}`}>F</div>
-                    <div id='g' className={`${style.key} ${style.keyOrange}`}>G</div>
-                    <div id='h' className={`${style.key} ${style.keyYellow}`}>H</div>
-                    <div id='j' className={`${style.key} ${style.keyYellow}`}>J</div>
-                    <div id='k' className={`${style.key} ${style.keyPink}`}>K</div>
-                    <div id='l' className={`${style.key} ${style.keyBlue}`}>L</div>
-                    <div id=';' className={`${style.key} ${style.keyGreen}`}>;</div>
-                    <div id="'" className={`${style.key} ${style.keyGreen}`}>'</div>
-                    <div className={`${style.key} ${style.keyEnter} ${style.keyWaveBlue}`}>ENTER</div>
+                    <div className={`${style.key} ${style.keyCaps} ${style.FirstBlock}`}>CAPS</div>
+                    <div id='a' className={`${style.key} ${style.secondBlock}`}>A</div>
+                    <div id='s' className={`${style.key} ${style.thirdBlock}`}>S</div>
+                    <div id='d' className={`${style.key} ${style.fourthBlock}`}>D</div>
+                    <div id='f' className={`${style.key} ${style.fifthBlock}`}>F</div>
+                    <div id='g' className={`${style.key} ${style.fifthBlock}`}>G</div>
+                    <div id='h' className={`${style.key} ${style.sixthBlock}`}>H</div>
+                    <div id='j' className={`${style.key} ${style.sixthBlock}`}>J</div>
+                    <div id='k' className={`${style.key} ${style.fourthBlock}`}>K</div>
+                    <div id='l' className={`${style.key} ${style.thirdBlock}`}>L</div>
+                    <div id=';' className={`${style.key} ${style.secondBlock}`}>;</div>
+                    <div id="'" className={`${style.key} ${style.secondBlock}`}>'</div>
+                    <div className={`${style.key} ${style.keyEnter} ${style.FirstBlock}`}>ENTER</div>
                 </div>
                 <div className={style.row}>
-                    <div className={`${style.key} ${style.keyShift} ${style.keyWaveBlue}`}>SHIFT</div>
-                    <div id='z' className={`${style.key} ${style.keyGreen}`}>Z</div>
-                    <div id='x' className={`${style.key} ${style.keyBlue}`}>X</div>
-                    <div id='c' className={`${style.key} ${style.keyPink}`}>C</div>
-                    <div id='v' className={`${style.key} ${style.keyOrange}`}>V</div>
-                    <div id='b' className={`${style.key} ${style.keyOrange}`}>B</div>
-                    <div id='n' className={`${style.key} ${style.keyYellow}`}>N</div>
-                    <div id='m' className={`${style.key} ${style.keyYellow}`}>M</div>
-                    <div id=',' className={`${style.key} ${style.keyPink}`}>,</div>
-                    <div id='.' className={`${style.key} ${style.keyBlue}`}>.</div>
-                    <div id='/' className={`${style.key} ${style.keyGreen}`}>/</div>
-                    <div className={`${style.key} ${style.keyShift} ${style.keyWaveBlue}`}>SHIFT</div>
+                    <div className={`${style.key} ${style.keyShift} ${style.FirstBlock}`}>SHIFT</div>
+                    <div id='z' className={`${style.key} ${style.secondBlock}`}>Z</div>
+                    <div id='x' className={`${style.key} ${style.thirdBlock}`}>X</div>
+                    <div id='c' className={`${style.key} ${style.fourthBlock}`}>C</div>
+                    <div id='v' className={`${style.key} ${style.fifthBlock}`}>V</div>
+                    <div id='b' className={`${style.key} ${style.fifthBlock}`}>B</div>
+                    <div id='n' className={`${style.key} ${style.sixthBlock}`}>N</div>
+                    <div id='m' className={`${style.key} ${style.sixthBlock}`}>M</div>
+                    <div id=',' className={`${style.key} ${style.fourthBlock}`}>,</div>
+                    <div id='.' className={`${style.key} ${style.thirdBlock}`}>.</div>
+                    <div id='/' className={`${style.key} ${style.secondBlock}`}>/</div>
+                    <div className={`${style.key} ${style.keyShift} ${style.FirstBlock}`}>SHIFT</div>
                 </div>
                 <div className={style.row}>
-                    <div id=' ' className={`${style.key} ${style.keySpace} ${style.keyWaveBlue}`}> </div>
+                    <div id=' ' className={`${style.key} ${style.keySpace} ${style.FirstBlock}`}></div>
                 </div>
             </div>
         )
@@ -93,7 +93,7 @@ class Keyboard extends React.Component {
 
 Keyboard.propTypes = {
     lastLetter: PropTypes.string,
-    mistakeHappend: PropTypes.bool
+    mistakeHappened: PropTypes.bool
 }
 
 export default Keyboard;
