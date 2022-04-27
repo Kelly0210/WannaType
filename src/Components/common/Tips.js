f3ee24158a9e5272f0b6dc6972c5e3411bc81bd5
Alan Keller <52039446+Kelly0210@users.noreply.github.com>
2022-04-27 22:29:58 +0600

    add tips

--- src/Components/common/Tips.js
+++ src/Components/common/Tips.js
@@ -1,6 +1,16 @@
 export const tips = [
-    `Don't look down your hands 👀`,
     'Maintain good posture 🙇',
+    "Don't look down your hands 👀",
     'Find a comfortable position for your hands ⌨️',
-    'Practice, Practice, Practice! 🏃',
+    'Start Slowly With Touch-Typing 🐌',
+    
+    'Practice, Practice, Practice! 👾',
+    'Take Breaks 🏃',
+    "Don't Hurry 😵",
+
+    'Remember colored areas 🤹',
+    'Track your typos 🔍',
+    'Memorize the following letters 🤖',
+
+    'Listen Background Music 🎵',
 ]
