857a5672087bad8d316c4aa2e320d7c18ede1ba5
Alan Keller <52039446+Kelly0210@users.noreply.github.com>
2023-06-05 21:54:11 +0600

    build(?)

---
+++ src/Components/Chapters/Chapters.jsx
@@ -0,0 +1,117 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from "react";
+import { Link } from "react-router-dom";
+import style from './Chapters.module.css';
+import collapseArrow from '../../assets/img/collapseArrow.png';
+import { textCollection } from "../../common/textCollection";
+import { lessonsCollection } from '../../common/LessonsCollection';
+import { generateLesson } from "../../common/generateLesson";
+
+const Chapters = (props) => {
+    const entireComponent = React.createRef();
+    const collapseIcon = React.createRef();
+
+    const createLessonsCollection = (allLessons) => {
+        let collection = [];
+
+        Object.values(allLessons).map((lesson, i) => collection.push(
+            <React.Fragment key={`Lesson ${i}`}>
+                 {/* Пофиксить li  */}
+                <li onClick={toggleList} className={style.toggleList}>{`Lesson ${++i}`}</li>
+                <ul id={`Lesson ${i}`} className={style.innerUl}>
+                    {createChaptersCollection(lesson, `lesson${i}`)}
+                </ul>
+                
+            </React.Fragment>
+        ))
+        return collection;
+    }
+
+    const createChaptersCollection = (lesson, lessonNumber) => {
+        let collection = [];
+
+        Object.values(lesson).map((chapter, i) => collection.push(
+            <li key={chapter.url}>
+                <Link to={chapter.url}
+                    onClick={() => lessonClick(chapter, lessonNumber, `chapter${++i}`)}
+                    dangerouslySetInnerHTML={{ __html: chapter.title }} />
+            </li>
+        ))
+        return collection;
+    }
+
+    const lessonClick = (chapter, lessonNumber, chapterNumber) => {
+        props.passGeneratedLesson({
+            lessonInfo: {
+                type: 'lesson',
+                units: chapter.units,
+                numberOfUnits: chapter.numberOfUnits,
+                lesson: lessonNumber,
+                chapter: chapterNumber,
+            },
+            title: chapter.title,
+            generatedText: generateLesson(chapter.units, chapter.numberOfUnits)
+        });
+    }
+
+    const randomTextClick = () => {
+        let randomText = textCollection[Math.floor(Math.random() * textCollection.length)];
+
+        props.passGeneratedLesson({
+            lessonInfo: {
+                type: 'random-text'
+            },
+            title: 'Random Text',
+            generatedText: randomText
+        });
+    }
+
+    const randomExerciseClick = () => {
+        const allLessons = Object.keys(lessonsCollection);
+        const randomLesson = allLessons[Math.floor(Math.random() * allLessons.length)];
+        const foundLesson = lessonsCollection[randomLesson];
+
+        const allChapters = Object.keys(foundLesson);
+        const randomChapter = allChapters[Math.floor(Math.random() * allChapters.length)];
+        const foundChapter = foundLesson[randomChapter];
+
+        props.passGeneratedLesson({
+            lessonInfo: {
+                type: 'random-exercise',
+                units: foundChapter.units,
+                numberOfUnits: foundChapter.numberOfUnits
+            },
+            title: foundChapter.title,
+            generatedText: generateLesson(foundChapter.units, foundChapter.numberOfUnits)
+        });
+    }
+
+    const toggleList = (event) => {
+        let innerUl = document.getElementById(event.target.textContent);
+        innerUl.classList.toggle(`${style.show}`);
+
+        event.target.classList.toggle(`${style.active}`);
+    }
+
+    const collapseComponent = () => {
+        entireComponent.current.classList.toggle(`${style.hide}`);
+        collapseIcon.current.classList.toggle(`${style.show}`);
+    }
+
+    return (
+        <>
+            <img src={collapseArrow} className={style.collapseRight} onClick={collapseComponent} alt="collapse icon" ref={collapseIcon} />
+
+            <nav ref={entireComponent} className={style.chaptersContainer}>
+                <img src={collapseArrow} className={style.collapseLeft} onClick={collapseComponent} alt="collapse icon" />
+                <ul>
+                    {createLessonsCollection(lessonsCollection)}
+                    <li><Link to='/random-exercise' onClick={randomExerciseClick}>Random Exercise</Link></li>
+                    <li><Link to='/random-text' onClick={randomTextClick}>Random Text</Link></li>
+                </ul>
+            </nav >
+        </>
+    )
+}
+
+export default Chapters;
---
+++ src/Components/CustomInput/CustomInput.jsx
@@ -0,0 +1,100 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import FinishBoard from '../Board/FinishBoard';
+import DisplayText from '../Display/DisplayText';
+import style from './CustomInput.module.css';
+
+const CustomInput = (props) => {
+    const [isDisabled, disableInput] = React.useState(false);
+    const [isPassed, passedResult] = React.useState(false);
+    const [startTimeMs, setStopwatch] = React.useState(0);
+    const [value, changeValue] = React.useState('');
+
+    React.useEffect(() => {
+        if (value.length !== 0) {
+            props.passLastLetter(value.slice(-1));
+        }
+    }, [value]);
+
+    React.useEffect(() => {
+        if (value.slice(-1) !== props.generatedLesson.generatedText[value.length - 1]) {
+            props.mistakeCounter();
+            props.mistakeHappenedToggle(true);
+        } else {
+            props.mistakeHappenedToggle(false);
+        }
+
+        if (props.numberOfMistakes === 3) {
+            disableInput(true);
+            passedResult(false);
+        }
+    }, [value]);
+
+    React.useEffect(() => {
+        if (value.length === 1) {
+            setStopwatch(new Date().getTime());
+        }
+        if (value.length !== 0) {
+            let timePassedMs = new Date().getTime();
+            let timePassedMin = (timePassedMs - startTimeMs) / 1000 / 60;
+            let CPM = Math.ceil(value.length / timePassedMin);
+
+            props.charactersPerMinute(CPM);
+        }
+    }, [value.length]);
+
+    React.useEffect(() => {
+        if (value.length === props.generatedLesson.generatedText.length && value.length > 0) {
+            passedResult(true);
+            disableInput(true);
+        }
+    }, [value.length, props.generatedLesson.generatedText.length]);
+
+    React.useEffect(() => {
+        let percentage = Math.ceil(value.length / props.generatedLesson.generatedText.length * 100);
+        props.completionPercentage(percentage);
+    });
+
+    const reloadLesson = (lessonString, chapterString) => {
+        setStopwatch(0);
+        disableInput(false);
+        passedResult(false);
+        changeValue('');
+
+        props.reloadLesson(lessonString, chapterString);
+    };
+
+    return (
+        <div className={style.InputBox}>
+            <DisplayText value={value}
+                generatedText={props.generatedLesson.generatedText} />
+
+            <input autoFocus
+                type="text"
+                value={value}
+                disabled={isDisabled}
+                onChange={event => changeValue(event.target.value)} />
+
+            {isDisabled && <FinishBoard isPassed={isPassed}
+                lessonInfo={props.generatedLesson.lessonInfo}
+                reloadLesson={reloadLesson}
+            />}
+        </div>
+    )
+};
+
+CustomInput.propTypes = {
+    generatedText: PropTypes.string,
+    reloadLesson: PropTypes.func,
+
+    numberOfMistake: PropTypes.number,
+    handleMistake: PropTypes.func,
+    mistakeCounter: PropTypes.func,
+
+    completionPercentage: PropTypes.func,
+    charactersPerMinute: PropTypes.func,
+    passLastLetter: PropTypes.func
+};
+
+export default CustomInput;
